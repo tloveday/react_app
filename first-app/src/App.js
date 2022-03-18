36e6f4f3e362690c4ff0a49fdcdb7b0c5869b4bb
tloveday <thomas_loveday848@hotmail.com>
2022-03-18 13:15:52 +0000

    start of react app tutorial

---
+++ first-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreeting from './components/FunctionalGreeting';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreeting />
+    </div>
+  );
+}
+
+export default App;
---
+++ first-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+//function FunctionalGreeting(){
+    //        return <h1>Hello from React!</h1>
+    //}
+
+const FunctionalGreeting = () => <h1>Hi there! </h1>
+export default FunctionalGreeting;
